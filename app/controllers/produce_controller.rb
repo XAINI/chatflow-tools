c0532e504d2443445fd37ca175f5532efd860c11
arlyxiao <kingla_pei@163.com>
2015-02-15 14:03:51 +0800

    fix convert

--- app/controllers/produce_controller.rb
+++ app/controllers/produce_controller.rb
@@ -16,8 +16,7 @@ class ProduceController < ApplicationController
     
 
     target_file = Rails.root.to_s + '/public/target.yml'
-    file_path = Rails.root.to_s + '/public/origin.yml'
-    yml_file = YAML::load_file(file_path)
+    pre_path = Rails.root.to_s + '/public/pre.yml'
 
 
     File.open(target_file, 'w') {}
@@ -29,22 +28,28 @@ class ProduceController < ApplicationController
     items = str.scan(/\[(.*?)\]([^\[]*)/)
 
 
+    @data = {}
+    @data['scripts'] = []
     items.each_with_index do |item, index|
       # p index
       # p item[0]
       # p item[1]
 
+      yml_file = YAML::load_file(pre_path)
+
       item[1] = item[1].gsub(/^#{item[0]}/, '')
       yml_file['scripts'][0]['npc'] = item[0]
       yml_file['scripts'][0]['sentences'][0]['text'] = item[1]
 
+      @data['scripts'] << yml_file['scripts'][0]
 
-      File.open(target_file, 'a') {|f| f.write yml_file.to_yaml }
+      
+      # File.open(target_file, 'a') {|f| f.write yml_file.to_yaml }
 
     end
 
-    @output = File.read(target_file)
-
+    # File.open(target_file, 'w') {|f| f.write @data.to_yaml }
+    # @output = File.read(target_file)
 
     render :index
     # render :nothing => true
