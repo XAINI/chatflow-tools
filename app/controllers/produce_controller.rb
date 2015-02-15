8456a58ee5ef01e8b4bc6dbfd869be63599ecc3e
arlyxiao <kingla_pei@163.com>
2015-02-15 16:27:50 +0800

    add qq support

--- app/controllers/produce_controller.rb
+++ app/controllers/produce_controller.rb
@@ -13,49 +13,72 @@ class ProduceController < ApplicationController
 
 
   def convert
-    
 
-    target_file = Rails.root.to_s + '/public/target.yml'
     pre_path = Rails.root.to_s + '/public/pre.yml'
 
-
-    File.open(target_file, 'w') {}
-
-    str = params[:chat_text]
-
-
-    str = str.gsub(/\d{2}\:\d{2}/, '').gsub(/\*/, '').gsub(/\n/,'').gsub(/@/, '').gsub(/:/, '').gsub(/\r/, '')
-    items = str.scan(/\[(.*?)\]([^\[]*)/)
-
-
-    @data = {}
-    @data['scripts'] = []
-    items.each_with_index do |item, index|
-      # p index
-      # p item[0]
-      # p item[1]
-
-      yml_file = YAML::load_file(pre_path)
-
-      item[1] = item[1].gsub(/^#{item[0]}/, '')
-      yml_file['scripts'][0]['npc'] = item[0]
-      yml_file['scripts'][0]['sentences'][0]['text'] = item[1]
-
-      @data['scripts'] << yml_file['scripts'][0]
-
-      
-      # File.open(target_file, 'a') {|f| f.write yml_file.to_yaml }
-
+    case params[:type]
+    when 'qq'
+      @data = convert_qq(params[:chat_text], pre_path)
+    else
+      @data = convert_webchat(params[:chat_text], pre_path)
     end
-
-    # File.open(target_file, 'w') {|f| f.write @data.to_yaml }
-    # @output = File.read(target_file)
+    
 
     render :index
     # render :nothing => true
 
-
   end
+
+
+  private
+    def convert_webchat(str, pre_path)
+
+      str = str.gsub(/\d{2}\:\d{2}/, '').gsub(/\*/, '').gsub(/\n/,'').gsub(/@/, '').gsub(/:/, '').gsub(/\r/, '')
+      items = str.scan(/\[(.*?)\]([^\[]*)/)
+
+
+      data = {}
+      data['scripts'] = []
+      items.each_with_index do |item, index|
+
+        yml_file = YAML::load_file(pre_path)
+
+        item[1] = item[1].gsub(/^#{item[0]}/, '')
+        yml_file['scripts'][0]['npc'] = item[0]
+        yml_file['scripts'][0]['sentences'][0]['text'] = item[1]
+
+        data['scripts'] << yml_file['scripts'][0]
+
+        
+      end
+
+      data
+    end
+
+
+    def convert_qq(str, pre_path)
+      pattern = /^[\u4E00-\u9FA5\w]+[\s][\d{4}\/\d{1,2}\/\d{1,2} \d{2}:\d{2}:\d{2}]{1,}/
+      items = str.scan(pattern)
+      values = str.split(pattern).delete_if(&:blank?)
+
+      data = {}
+      data['scripts'] = []
+      items.each_with_index do |item, index|
+
+        yml_file = YAML::load_file(pre_path)
+
+        yml_file['scripts'][0]['npc'] = items[index].gsub(/\d{4}\/\d{1,2}\/\d{1,2} \d{2}:\d{2}:\d{2}/, '').strip
+
+        value = values[index].gsub(/\r\n/,'').gsub(/@/, '').gsub(/:/, '').gsub(/\r/, '')
+        yml_file['scripts'][0]['sentences'][0]['text'] = value
+
+        data['scripts'] << yml_file['scripts'][0]
+
+        
+      end
+
+      data
+    end
 
 
 
